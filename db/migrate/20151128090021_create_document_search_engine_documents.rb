eef0c52e841721c1b5120426d7be707036cb3b33
Masafumi Yokoyama <myokoym@gmail.com>
2015-11-28 18:02:47 +0900

    Use Mroonga

--- db/migrate/20151128090021_create_document_search_engine_documents.rb
+++ db/migrate/20151128090021_create_document_search_engine_documents.rb
@@ -1,6 +1,6 @@
 class CreateDocumentSearchEngineDocuments < ActiveRecord::Migration
   def change
-    create_table :document_search_engine_documents do |t|
+    create_table :document_search_engine_documents, options: "ENGINE=Mroonga" do |t|
       t.text :title
       t.text :content
 
