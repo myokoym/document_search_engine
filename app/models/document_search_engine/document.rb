de6ff5f7982a879bf57066848e89890fc9c23511
Masafumi Yokoyama <myokoym@gmail.com>
2015-11-28 18:29:55 +0900

    Add full_text_search scope

--- app/models/document_search_engine/document.rb
+++ app/models/document_search_engine/document.rb
@@ -1,4 +1,7 @@
 module DocumentSearchEngine
   class Document < ActiveRecord::Base
+    scope :full_text_search, -> (query) {
+      where("MATCH(content) AGAINST(? IN BOOLEAN MODE)", "*D+ #{query}")
+    }
   end
 end
